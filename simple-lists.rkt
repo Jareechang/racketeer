4dca5b7dff6793b1b8724d8f28bf8dffddaa7cc8
Jerry Chang <therealjerrycan@gmail.com>
2017-02-26 17:06:16 -0800

    added code about recursive list

--- simple-lists.rkt
+++ simple-lists.rkt
@@ -3,7 +3,30 @@
 (require 
   "lib/display-with-newline.rkt")
 
-; append a list
+(define hop-skip (list "hop" "skip"))
+(define jump (list "jump"))
 
+; append a list
 (display-with-newline
-  (append (list "hop" "skip") (list "jump")))
+  (append hop-skip jump))
+
+; reverse
+(display-with-newline
+  (reverse (append hop-skip jump)))
+
+; Lambdas 
+(map sqrt (list 1 4 9 16))
+
+; Add exclamation mark with lambdas
+(map (lambda (i)
+       (string-append i "!"))
+     (list "hop" "skip" "jump"))
+
+; Recursive (my-)length 
+
+(define (my-length lst)
+  (cond 
+    [(empty? lst) 0]
+    [else (+ (my-length (rest lst)) 1)]))
+
+(my-length (list "hello" "wowza" "hey"))
